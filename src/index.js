b5e8dff2d4b9f15c1420035d4716425572191290
Luzinda Douglas <douglas.luzinda.ot@gmail.com>
2022-11-24 09:05:31 +0300

    Leaderboard setup

--- src/index.js
+++ src/index.js
@@ -1,2 +1,14 @@
 import _ from 'lodash';
 import './style.css';
+
+class scores{
+    constructor(Name,score) {
+        this.Name=Name
+        this.score=score
+    }
+}
+
+const addItems=()=>{
+
+   
+}
